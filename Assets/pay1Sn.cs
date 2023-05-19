9aac1f176195e1fefbc33410776058accd09b6e5
gityGuy <134052387+gityGuy@users.noreply.github.com>
2023-05-19 14:50:00 +0530

    all project files
    it contains all assets and script for this project

---
+++ Assets/pay1Sn.cs
@@ -0,0 +1,14 @@
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class pay1Sn : MonoBehaviour
+{
+    public void LoadScene()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+}
